89a68e986a645be616386a279cba1499d9b20662
Animenzzz <kangjian_037@126.com>
2019-12-06 15:59:32 +0800

    --新增：setting页

---
+++ ANKPoke/ANKPoke/Setting/SettingRootView.swift
@@ -0,0 +1,21 @@
+//
+//  SettingRootView.swift
+//  ANKPoke
+//
+//  Created by Animenzzz on 2019/12/6.
+//  Copyright © 2019 Animenzzz. All rights reserved.
+//
+
+import SwiftUI
+
+struct SettingRootView: View {
+    var body: some View {
+        SettingView().navigationBarTitle("设置")
+    }
+}
+
+struct SettingRootView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingRootView()
+    }
+}
